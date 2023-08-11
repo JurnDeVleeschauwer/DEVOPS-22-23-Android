7b7728cc47dba000a9ca91a83001e26ac252f0ab
jurnDeVleeschau <61053327+jurnDeVleeschau@users.noreply.github.com>
2023-08-11 16:14:02 +0200

    to disable ssl verification

--- app/src/main/java/com/hogent/devOps_Android/network/VmApiService.kt
+++ app/src/main/java/com/hogent/devOps_Android/network/VmApiService.kt
@@ -4,10 +4,19 @@ import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterF
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.Deferred
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import javax.net.ssl.SSLContext
+import javax.net.ssl.SSLSocketFactory
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
+import javax.security.cert.CertificateException
+
 
 private const val BASE_URL = "https://10.0.2.2:44356/api/"
 
@@ -19,6 +28,7 @@ private val retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory.create(moshi))
     .addCallAdapterFactory(CoroutineCallAdapterFactory())
     .baseUrl(BASE_URL)
+    .client(getUnsafeOkHttpClient())
     .build()
 interface VmApiService {
     @GET("project/User")
@@ -41,5 +51,48 @@ interface VmApiService {
 object VmApi {
     val retrofitService : VmApiService by lazy {
         retrofit.create(VmApiService::class.java)
+    }
+}
+
+private fun getUnsafeOkHttpClient(): OkHttpClient? {
+    return try {
+        // Create a trust manager that does not validate certificate chains
+        val trustAllCerts =
+            arrayOf<TrustManager>(
+                object : X509TrustManager {
+                    @Throws(CertificateException::class)
+                    override fun checkClientTrusted(
+                        chain: Array<X509Certificate>,
+                        authType: String
+                    ) {
+                    }
+
+                    @Throws(CertificateException::class)
+                    override fun checkServerTrusted(
+                        chain: Array<X509Certificate>,
+                        authType: String
+                    ) {
+                    }
+
+                    override fun getAcceptedIssuers(): Array<X509Certificate> {
+                        return arrayOf()
+                    }
+                }
+            )
+
+        // Install the all-trusting trust manager
+        val sslContext = SSLContext.getInstance("SSL")
+        sslContext.init(null, trustAllCerts, SecureRandom())
+        // Create an ssl socket factory with our all-trusting manager
+        val sslSocketFactory: SSLSocketFactory = sslContext.socketFactory
+        val builder = OkHttpClient.Builder()
+        builder.sslSocketFactory(
+            sslSocketFactory,
+            (trustAllCerts[0] as X509TrustManager)
+        )
+        builder.hostnameVerifier { hostname, session -> true }
+        builder.build()
+    } catch (e: Exception) {
+        throw RuntimeException(e)
     }
 }
