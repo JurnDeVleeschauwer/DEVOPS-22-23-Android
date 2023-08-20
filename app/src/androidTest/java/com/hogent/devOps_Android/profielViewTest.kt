d65f5fa97b7922d52d41fa8711233149fd4f3647
jurnDeVleeschau <61053327+jurnDeVleeschau@users.noreply.github.com>
2023-08-20 09:47:22 +0200

    trying to get more test with espresso

--- app/src/androidTest/java/com/hogent/devOps_Android/profielViewTest.kt
+++ app/src/androidTest/java/com/hogent/devOps_Android/profielViewTest.kt
@@ -1,4 +1,4 @@
-package com.hogent.devOps_Android.screens
+package com.hogent.devOps_Android
 
 import android.R
 import android.widget.TextView
@@ -12,6 +12,7 @@ import androidx.test.espresso.intent.Intents
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.rule.ActivityTestRule
 import com.hogent.devOps_Android.app.MainActivity
 import com.hogent.devOps_Android.ui.klant.CustomerProfileFragment
@@ -22,7 +23,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 
-@RunWith(AndroidJUnit4::class)
+@RunWith(AndroidJUnit4ClassRunner::class)
 class profielViewTest {
 
         @get:Rule
@@ -48,6 +49,8 @@ class profielViewTest {
             //startFragment(fragment);
             fragment.moveToState(Lifecycle.State.RESUMED)
 
+            //onView(withId(R.id.))
+
 
             fragment.onFragment {
                 assert(it.view?.findViewById<TextView>(R.id.text1)?.text.toString() == "Hello From Blank")
@@ -60,14 +63,10 @@ class profielViewTest {
         @Test
         fun startVisibleFragment_shouldStartFragment() {
 
-            val transaction: FragmentTransaction =
-                activityScenarioRule.activity.supportFragmentManager.beginTransaction()
-            val customerProfileFragment = CustomerProfileFragment()
-            transaction.add(customerProfileFragment, "customerProfileFragment")
-            transaction.commit()
+            val customerProfileFragment = startMyFragment()
 
 
-            //onView(withId(R.id.))
+                //onView(withId(R.id.))
 
 
         }
@@ -85,8 +84,57 @@ class profielViewTest {
         fun startVisibleFragment_shouldAttachFragmentToActivity() {
             activityScenarioRule.getActivity()
                 .runOnUiThread(Runnable { val voiceFragment: CustomerProfileFragment = startMyFragment() })
-            // Then use Espresso to test the Fragment
+
             // Then use Espresso to test the Fragment
             //onView(withId(R.id.)).check(matches(isDisplayed()))
+        }/*
+
+        @Test
+        fun startFragment_shouldStartFragmentWithSpecifiedActivityClass() {
+            final LoginFragment fragment = new LoginFragment();
+            startFragment(fragment, LoginActivity.class);
+
+            assertThat(fragment.getView()).isNotNull();
+            assertThat(fragment.getActivity()).isNotNull();
+            assertThat((TextView) fragment.getView().findViewById(R.id.tacos)).isNotNull();
+            assertThat(fragment.getActivity()).isInstanceOf(LoginActivity.class);
         }
+
+        @Test
+        fun startVisibleFragment_shouldStartFragmentWithSpecifiedActivityClass() {
+            final LoginFragment fragment = new LoginFragment();
+            startVisibleFragment(fragment, LoginActivity.class, 1);
+
+            assertThat(fragment.getView()).isNotNull();
+            assertThat(fragment.getActivity()).isNotNull();
+            assertThat((TextView) fragment.getView().findViewById(R.id.tacos)).isNotNull();
+            assertThat(fragment.getActivity()).isInstanceOf(LoginActivity.class);
+        }
+
+        @Test
+        fun startVisibleFragment_shouldAttachFragmentToActivityWithSpecifiedActivityClass() {
+            final LoginFragment fragment = new LoginFragment();
+            startVisibleFragment(fragment, LoginActivity.class, 1);
+
+            assertThat(fragment.getView().getWindowToken()).isNotNull();
+            assertThat(fragment.getActivity()).isInstanceOf(LoginActivity.class);
+        }
+
+        public static class LoginFragment extends Fragment {
+            @Override
+            public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+                return inflater.inflate(R.layout.fragment_contents, container, false);
+            }
+        }
+
+        fun class LoginActivity extends Activity {
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                LinearLayout view = new LinearLayout(this);
+                view.setId(1);
+
+                setContentView(view);
+            }
+        }*/
 }
